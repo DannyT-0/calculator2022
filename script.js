90c8579a0697be4965ab227844beeb6619f3f1d0
danny <dannytokarev@gmail.com>
2022-04-07 12:32:17 -0400

    finished up basic calculator functionality with one operation at a time.

--- script.js
+++ script.js
@@ -15,32 +15,33 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-	if (operator == add) {
+	console.log(operator);
+	console.log(a);
+	console.log(b);
+	if (operator === "+") {
+		calcDisplay.textContent += " " + "=" + add(a, b);
 		return add(a, b);
-	} else if (operator == subtract) {
+	} else if (operator === "-") {
+		calcDisplay.textContent += " " + "=" + subtract(a, b);
 		return subtract(a, b);
-	} else if (operator == divide) {
+	} else if (operator === "/") {
+		calcDisplay.textContent += " " + "=" + divide(a, b);
 		return divide(a, b);
-	} else if (operator == multiply) {
+	} else if (operator === "*") {
+		calcDisplay.textContent += " " + "=" + multiply(a, b);
 		return multiply(a, b);
 	}
 }
 
-// const addButton = document.querySelector("#btnAdd");
-// const subtractButton = document.querySelector("#btnSubtract");
-// const multiplyButton = document.querySelector("#btnMultiply");
-// const divideButton = document.querySelector("#btnDivide");
 const operatorButtons = document.querySelectorAll(".opBtn");
 const numberButtons = document.querySelectorAll(".numBtn");
 const calcDisplay = document.querySelector(".display");
 const clearButton = document.querySelector("#btnClear");
 const equalButton = document.querySelector("#btnEqual");
 
-// equalButton.addEventListener("click", () => {
-//     if(calcDisplay)
-// });
-
 let currVal = 0;
+let secondVal = 0;
+let currentOperator = "";
 
 clearButton.addEventListener("click", () => {
 	calcDisplay.textContent = "";
@@ -49,12 +50,43 @@ clearButton.addEventListener("click", () => {
 numberButtons.forEach((btn) => {
 	btn.addEventListener("click", (e) => {
 		calcDisplay.textContent += " " + e.target.textContent;
-		currVal += e.target.textContent;
+
+		if (currVal == 0 && currentOperator == "") {
+			currVal = parseInt(e.target.textContent);
+		} else if (currVal >= 0 && currentOperator == "") {
+			currentOperator = e.target.textContent;
+		} else if (currVal >= 0 && currentOperator != "") {
+			secondVal = parseInt(e.target.textContent);
+		}
+
+		console.log(currVal, "currVal", typeof currVal);
+		console.log(secondVal, "secondVal");
 	});
 });
 
 operatorButtons.forEach((btn) => {
 	btn.addEventListener("click", (e) => {
 		calcDisplay.textContent += " " + e.target.textContent;
+		currentOperator = e.target.textContent;
+		console.log(currentOperator);
+		if (currentOperator == "+") {
+			currentOperator == add;
+		} else if (currentOperator == "-") {
+			currentOperator == subtract;
+		} else if (currentOperator == "*") {
+			currentOperator == multiply;
+		} else if (currentOperator == "/") {
+			currentOperator == divide;
+		}
 	});
 });
+
+equalButton.addEventListener("click", () => {
+	operate(currentOperator, currVal, secondVal);
+});
+
+// if the display is empty, click a number to start the calculation
+// if first number is satisfactory, click an operator to begin a calculation
+// after operator is clicked, enter another number
+// if that's the entire calculation, press enter for result, otherwise, click another operator
+// if another operator is clicked, first calculation result should be in display
